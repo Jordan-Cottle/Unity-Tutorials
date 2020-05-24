8ce9f58a03327e66fa4285b82ddf53d2e90bbe2e
Jordan-Cottle <jordancottle622@gmail.com>
2020-05-23 22:40:21 -0400

    Basic setup for procedural mesh

---
+++ Assets/Scrips/ProceduralGrid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(MeshFilter), typeof(MeshRenderer))]
+public class ProceduralGrid : MonoBehaviour
+{
+
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+
+    // Grid settings
+    public float cellSize;
+    public Vector3 gridOffset;
+    public int gridSize;
+
+    void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
