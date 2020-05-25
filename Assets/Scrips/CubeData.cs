33cdb9561ad2c54551e186934d0caaa025fa7ee3
Jordan-Cottle <jordancottle622@gmail.com>
2020-05-24 23:52:31 -0400

    Colorful cube with dynamic size
    End of part 8

---
+++ Assets/Scrips/CubeData.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class CubeData : MonoBehaviour
+{
+    public static Vector3[] vertices = new Vector3[] {
+        new Vector3(0, 1, 0),
+        new Vector3(0, 1, 1),
+        new Vector3(1, 1, 1),
+        new Vector3(1, 1, 0),
+
+        new Vector3(0, 0, 0),
+        new Vector3(0, 0, 1),
+        new Vector3(1, 0, 1),
+        new Vector3(1, 0, 0),
+    };
+
+    public static int[][] faceTriangles = {
+        // Top
+       new int [] {0, 1, 2, 3},
+       // Front
+       new int [] {4, 0, 3, 7},
+       // Back
+       new int [] {5, 6, 2, 1},
+       // Left
+       new int [] {5, 1, 0, 4},
+       // Right
+       new int [] {7, 3, 2, 6},
+
+       // Bottom
+       new int [] {6, 5, 4, 7},
+    };
+
+    public static Vector3[] FaceVertices(int face, float size = 1)
+    {
+        Vector3[] faceValues = new Vector3[4];
+        for (int i = 0; i < faceValues.Length; i++)
+        {
+            faceValues[i] = vertices[faceTriangles[face][i]] * size;
+        }
+
+        return faceValues;
+    }
+
+    void Update()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+        mesh.vertices = vertices;
+
+        mesh.triangles = new int[] {
+            // Top
+            0, 1, 2, 2, 3, 0,
+
+            // Bottom
+            6, 5, 4, 4, 7, 6
+        };
+
+        Color[] colors = new Color[vertices.Length];
+
+        colors[0] = new Color(0, 0, 0); // Black
+        colors[1] = new Color(0, 0, 1); // Blue
+        colors[2] = new Color(0, 1, 0); // Green
+        colors[3] = new Color(0, 1, 1); // Cyan
+        colors[4] = new Color(1, 0, 0); // Red
+        colors[5] = new Color(1, 0, 1); // Violet
+        colors[6] = new Color(1, 1, 0); // Yellow
+        colors[7] = new Color(1, 1, 1); // White
+
+        mesh.colors = colors;
+    }
+}
---
+++ Assets/Scrips/SimpleCube.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class SimpleCube : MonoBehaviour
+{
+
+    private Mesh mesh;
+    private List<Vector3> vertices;
+    private List<int> triangles;
+
+    private List<Color> colors;
+
+    public float size = 1;
+
+    void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        colors = new List<Color>();
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+
+        for (int i = 0; i < 24; i++)
+        {
+            colors.Add(
+                new Color(
+                    Random.Range(0, 255) / 255f,
+                    Random.Range(0, 255) / 255f,
+                    Random.Range(0, 255) / 255f
+                )
+            );
+        }
+    }
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        MakeCube();
+        UpdateMesh();
+    }
+
+    void MakeCube()
+    {
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+
+
+        for (int i = 0; i < 6; i++)
+        {
+            MakeFace(i);
+        }
+
+    }
+
+    void MakeFace(int face)
+    {
+        int vCount = vertices.Count;
+
+        triangles.Add(vCount + 0);
+        triangles.Add(vCount + 1);
+        triangles.Add(vCount + 2);
+        triangles.Add(vCount + 0);
+        triangles.Add(vCount + 2);
+        triangles.Add(vCount + 3);
+
+        vertices.AddRange(CubeData.FaceVertices(face, size));
+    }
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.colors = colors.ToArray();
+        mesh.RecalculateNormals();
+    }
+}
