ea473bedab1987c79af6290d5769cb765f30610c
Jordan-Cottle <jordancottle622@gmail.com>
2020-08-01 15:00:17 -0400

    Lesson 7: Board Manager

---
+++ Assets/Scripts/GameBoard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour
+{
+    private Building[,] buildings = new Building[100, 100];
+
+    public void AddBuilding(Building building, Vector3 position)
+    {
+        buildings[(int)position.x, (int)position.z] = Instantiate(building, position, Quaternion.identity);
+    }
+
+    public bool BuildingAt(Vector3 position)
+    {
+        return !(buildings[(int)position.x, (int)position.z] is null);
+    }
+
+    public Vector3 CalculateGridPosition(Vector3 position)
+    {
+        return new Vector3((int)position.x, 0, (int)position.z);
+    }
+}
